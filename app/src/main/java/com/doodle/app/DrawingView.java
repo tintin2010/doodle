4016fbf6afae8c60ddea9c3b4972c4388114100d
tintin2010 <seinjuti@gmail.com>
2014-06-30 10:34:05 +0530

    new repo for Doodle App
    	new file:   Doodle.iml
    	new file:   app/.gitignore
    	new file:   app/app.iml
    	new file:   app/build.gradle
    	new file:   app/proguard-rules.txt
    	new file:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/doodle/app/DrawingView.java
    	new file:   app/src/main/java/com/doodle/app/MainActivity.java
    	new file:   app/src/main/res/drawable-hdpi/eraser_img.png
    	new file:   app/src/main/res/drawable-hdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-hdpi/new_img.png
    	new file:   app/src/main/res/drawable-hdpi/paint.xml
    	new file:   app/src/main/res/drawable-hdpi/paint_img.png
    	new file:   app/src/main/res/drawable-hdpi/save_img.png
    	new file:   app/src/main/res/drawable-mdpi/eraser_img.png
    	new file:   app/src/main/res/drawable-mdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-mdpi/new_img.png
    	new file:   app/src/main/res/drawable-mdpi/paint.xml
    	new file:   app/src/main/res/drawable-mdpi/paint_img.png
    	new file:   app/src/main/res/drawable-mdpi/save_img.png
    	new file:   app/src/main/res/drawable-xhdpi/eraser_img.png
    	new file:   app/src/main/res/drawable-xhdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-xhdpi/new_img.png
    	new file:   app/src/main/res/drawable-xhdpi/paint.xml
    	new file:   app/src/main/res/drawable-xhdpi/paint_img.png
    	new file:   app/src/main/res/drawable-xhdpi/save_img.png
    	new file:   app/src/main/res/drawable-xxhdpi/eraser_img.png
    	new file:   app/src/main/res/drawable-xxhdpi/ic_launcher.png
        new file:   app/src/main/res/drawable-xxhdpi/new_img.png
    	new file:   app/src/main/res/drawable-xxhdpi/paint.xml
    	new file:   app/src/main/res/drawable-xxhdpi/paint_img.png
    	new file:   app/src/main/res/drawable-xxhdpi/save_img.png
    	new file:   app/src/main/res/layout/activity_main.xml
    	new file:   app/src/main/res/menu/main.xml
    	new file:   app/src/main/res/values-w820dp/dimens.xml
    	new file:   app/src/main/res/values/dimens.xml
    	new file:   app/src/main/res/values/strings.xml
    	new file:   app/src/main/res/values/styles.xml
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   local.properties
    	new file:   settings.gradle

---
+++ app/src/main/java/com/doodle/app/DrawingView.java
@@ -0,0 +1,19 @@
+package com.doodle.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by seinjutichatterjee on 6/29/14.
+ */
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+}
